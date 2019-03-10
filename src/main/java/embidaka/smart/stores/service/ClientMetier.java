b123880c0bf2bb3732353028c605b1ed03fbff02
Kevin Jonathan <jonathanhallykevin@gmail.com>
2019-03-10 23:17:26 +0400

    [Smart_store] : Ajout métier pour Client

---
+++ src/main/java/embidaka/smart/stores/service/ClientMetier.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package embidaka.smart.stores.service;
+
+import embidaka.smart.stores.models.Client;
+
+/**
+ *
+ * @author hjhonata
+ */
+public class ClientMetier {
+    public static Client login(String login, String motDePasse)
+    {
+        return null;
+    }
+}
